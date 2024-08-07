3194854cc72f89ac8d71bbb5fae9b7034060c31b
senorpickle <auskarson@gmail.com>
2024-08-07 17:08:06 +0700

    Delete samplepage/samplepage.js

--- samplepage/samplepage.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const formData = new FormData(this);
-    // Here you would typically send the form data to a server
-    console.log(Object.fromEntries(formData));
-});
